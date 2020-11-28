6203ce973481e700ccaa10093c6312965afb1e90
danielqsl <qiansl0316@126.com>
2020-11-28 22:07:02 +0800

    refactor:新增IP工具类

---
+++ src/main/java/com/company/project/utils/IpUtil.java
@@ -0,0 +1,36 @@
+package com.company.project.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author qianshuailong
+ * @date 2020/11/28
+ */
+@Slf4j
+public class IpUtil {
+
+    public static String getIpAddress(HttpServletRequest request) {
+        // 自定义：Header x-user-ip 记录PC，M站用户real-ip
+        if (StringUtils.isNotBlank(request.getHeader("x-user-ip"))) {
+            return request.getHeader("x-user-ip");
+        }
+        // Header x-real-ip 记录app用户
+        if (StringUtils.isNotBlank(request.getHeader("x-real-ip"))) {
+            return request.getHeader("x-real-ip");
+        }
+        // Header x-forwarded-for 记录多层代理服务器ip
+        if (StringUtils.isNotBlank(request.getHeader("x-forwarded-for"))) {
+            return request.getHeader("x-forwarded-for");
+        }
+        // Header remoteip 记录远程连接地址
+        if (StringUtils.isNotBlank(request.getHeader("remoteip"))) {
+            return request.getHeader("remoteip");
+        }
+        log.error("get ip from HttpServletRequest Header is null");
+        return StringUtils.EMPTY;
+    }
+
+}
