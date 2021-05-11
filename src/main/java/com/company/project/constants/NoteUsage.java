72af5d0601a14a0b50d60fb73d6b44a0e1917b8e
qianshuailong <shuailong.qian@weimob.com>
2021-05-11 16:46:04 +0800

    feat:注释使用方法

---
+++ src/main/java/com/company/project/constants/NoteUsage.java
@@ -0,0 +1,15 @@
+package com.company.project.constants;
+
+/**
+ * 注释使用
+ * <p>
+ * 跳转到指定类
+ * {@link LabelQueryTypeEnum}
+ *
+ * @author danielqsl
+ * <p>
+ * 跳转到指定类的指定方法
+ * @see LabelQueryTypeEnum#getCodeByDesc(String)
+ */
+public class NoteUsage {
+}
