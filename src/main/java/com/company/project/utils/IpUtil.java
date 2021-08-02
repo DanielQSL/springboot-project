4431ea3624db8509edfdc39d68b5e06a23aa8695
qianshuailong <shuailong.qian@weimob.com>
2021-08-02 18:27:34 +0800

    feat:IPv4转换为整型

--- src/main/java/com/company/project/utils/IpUtil.java
+++ src/main/java/com/company/project/utils/IpUtil.java
@@ -35,4 +35,41 @@ public class IpUtil {
         return StringUtils.EMPTY;
     }
 
+    // =========在保存IPv4地址时，一个IPv4最小需要7个字符，最大需要15个字符，而使用无符号整数来存储，只需要4个字节即可。============
+
+    /**
+     * 把字符串IP转换成long
+     * （仅限于IPv4）
+     *
+     * @param ipStr 字符串IP
+     * @return IP对应的long值
+     */
+    public static long ip2Long(String ipStr) {
+        String[] ip = ipStr.split("\\.");
+        return (Long.parseLong(ip[0]) << 24) + (Long.parseLong(ip[1]) << 16)
+                + (Long.parseLong(ip[2]) << 8) + Long.parseLong(ip[3]);
+    }
+
+    /**
+     * 把IP的long值转换成字符串
+     * （仅限于IPv4）
+     *
+     * @param ipLong IP的long值
+     * @return long值对应的字符串
+     */
+    public static String long2Ip(long ipLong) {
+        StringBuilder ip = new StringBuilder();
+        ip.append(ipLong >>> 24).append(".");
+        ip.append((ipLong >>> 16) & 0xFF).append(".");
+        ip.append((ipLong >>> 8) & 0xFF).append(".");
+        ip.append(ipLong & 0xFF);
+        return ip.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(ip2Long("192.168.0.1"));
+        System.out.println(long2Ip(3232235521L));
+        System.out.println(ip2Long("10.0.0.1"));
+    }
+
 }
