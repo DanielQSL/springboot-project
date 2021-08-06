22a775687028c5e41c1be7b6d6018eee62c9a61b
qianshuailong <shuailong.qian@weimob.com>
2021-08-06 14:47:59 +0800

    refactor:调整全局异常处理

--- src/main/java/com/company/project/handler/GlobalExceptionHandler.java
+++ src/main/java/com/company/project/handler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
-package com.company.project.exception;
+package com.company.project.handler;
 
 import com.company.project.common.CommonResponse;
+import com.company.project.enums.ResponseCodeEnum;
+import com.company.project.exception.BizException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -32,13 +34,13 @@ public class GlobalExceptionHandler {
      * @param ex 异常
      * @return 响应结果
      */
-    @ExceptionHandler(BindException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(BindException.class)
     public CommonResponse bindExceptionHandler(BindException ex) {
         String errorMsg = ex.getBindingResult().getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(";", "", ""));
-        return CommonResponse.fail(errorMsg);
+        return CommonResponse.fail(ResponseCodeEnum.BAD_REQUEST, errorMsg);
     }
 
     /**
@@ -47,13 +49,13 @@ public class GlobalExceptionHandler {
      * @param ex 异常
      * @return 响应结果
      */
-    @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
     public CommonResponse constraintViolationExceptionHandler(ConstraintViolationException ex) {
         String errorMsg = ex.getConstraintViolations().stream()
                 .map(ConstraintViolation::getMessage)
                 .collect(Collectors.joining(";", "", ""));
-        return CommonResponse.fail(errorMsg);
+        return CommonResponse.fail(ResponseCodeEnum.BAD_REQUEST, errorMsg);
     }
 
     /**
@@ -62,13 +64,13 @@ public class GlobalExceptionHandler {
      * @param ex 异常
      * @return 响应结果
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public CommonResponse methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException ex) {
         String errorMsg = ex.getBindingResult().getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(";", "", ""));
-        return CommonResponse.fail(errorMsg);
+        return CommonResponse.fail(ResponseCodeEnum.BAD_REQUEST, errorMsg);
     }
 
     /**
@@ -78,8 +80,8 @@ public class GlobalExceptionHandler {
      * @param ex      业务异常
      * @return 响应结果
      */
-    @ExceptionHandler(value = BizException.class)
     @ResponseStatus(HttpStatus.OK)
+    @ExceptionHandler(value = BizException.class)
     public CommonResponse bizExceptionHandler(HttpServletRequest request, final BizException ex) {
         log.warn("{} 业务异常", request.getRequestURI(), ex);
         return CommonResponse.fail(ex.getCode(), ex.getMessage());
@@ -92,11 +94,11 @@ public class GlobalExceptionHandler {
      * @param ex      异常
      * @return 响应结果
      */
-    @ExceptionHandler(value = Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
     public CommonResponse otherExceptionHandler(HttpServletRequest request, final Exception ex) {
         log.error("{} 系统异常", request.getRequestURI(), ex);
-        return CommonResponse.fail(ex.getMessage());
+        return CommonResponse.fail(ResponseCodeEnum.INTERNAL_SERVER_ERROR, ex.getMessage());
     }
 
 }
