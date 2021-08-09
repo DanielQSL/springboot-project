d2fa4f28da65bdbca2f257a2a1b83f81d28ca032
qianshuailong <shuailong.qian@weimob.com>
2021-08-09 10:57:33 +0800

    feat:新增健康检查接口

---
+++ src/main/java/com/company/project/controller/HealthCheckController.java
@@ -0,0 +1,38 @@
+package com.company.project.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 健康检测 Controller层
+ *
+ * @author danielqsl
+ */
+@Controller
+@RequestMapping("/")
+public class HealthCheckController {
+
+    /**
+     * 预热数据完成标志
+     */
+    public volatile boolean finishedFlag = false;
+
+    /**
+     * 健康检查接口
+     * （数据预热完成后再对外提供接口）
+     *
+     * @return 响应
+     */
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthCheck() {
+        if (finishedFlag) {
+            return ResponseEntity.ok("OK");
+        } else {
+            return new ResponseEntity<>("Waiting", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+}
