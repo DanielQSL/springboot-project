7632a80ba1ab2d80f21e1fcba7ce16e3e0ab27f7
qianshuailong <shuailong.qian@weimob.com>
2021-12-13 16:38:20 +0800

    feat:新增常量类

---
+++ src/main/java/com/company/project/constants/CoreConstants.java
@@ -0,0 +1,35 @@
+package com.company.project.constants;
+
+/**
+ * 框架核心的一些常量定义
+ *
+ * @author DanielQSL
+ */
+public interface CoreConstants {
+
+    /**
+     * 默认时区
+     */
+    String DEFAULT_TIME_ZONE = "Asia/Shanghai";
+
+    /**
+     * 统一的日期格式
+     */
+    String DATE_FORMAT_PATTERN = "yyyy-MM-dd";
+
+    /**
+     * 统一的日期时间格式
+     */
+    String DATE_TIME_FORMAT_PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 统一的时间格式
+     */
+    String TIME_FORMAT_PATTERN = "HH:mm:ss";
+
+    /**
+     * 格林威治时间格式
+     */
+    String GMT_FORMAT_PATTERN = "EEE MMM dd HH:mm:ss z yyyy";
+
+}
