675ebbf38d3cb404731acfb6c4c65272da5696d4
qianshuailong <shuailong.qian@weimob.com>
2022-06-06 16:29:07 +0800

    refactor:重构BaseEntity

--- src/main/java/com/company/project/dao/BaseEntity.java
+++ src/main/java/com/company/project/dao/BaseEntity.java
@@ -1,11 +1,10 @@
 package com.company.project.dao;
 
 import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
-import lombok.Data;
 
-import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -13,15 +12,12 @@ import java.util.Date;
  *
  * @author DanielQSL
  */
-@Data
-public abstract class BaseEntity implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+public class BaseEntity {
 
     /**
      * id
      */
-    @TableId
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
@@ -39,13 +35,53 @@ public abstract class BaseEntity implements Serializable {
     /**
      * 更新者
      */
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Long updateUser;
 
     /**
      * 更新时间
      */
-    @TableField(fill = FieldFill.INSERT)
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(Long createUser) {
+        this.createUser = createUser;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(Long updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
 
 }
