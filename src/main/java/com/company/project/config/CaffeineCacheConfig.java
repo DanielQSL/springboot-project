9974e9c1c7b2b7ae29f324e2805547b682500aaf
qianshuailong <shuailong.qian@weimob.com>
2022-10-19 18:47:26 +0800

    feat:guava cache

--- src/main/java/com/company/project/config/CaffeineCacheConfig.java
+++ src/main/java/com/company/project/config/CaffeineCacheConfig.java
@@ -2,6 +2,7 @@ package com.company.project.config;
 
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import com.google.common.cache.CacheBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,6 +27,22 @@ public class CaffeineCacheConfig {
                 .initialCapacity(200)
                 // 缓存的最大条数
                 .maximumSize(2000)
+                .build();
+    }
+
+    @Bean("guavaCache")
+    public com.google.common.cache.Cache<String, String> guavaCache() {
+        return CacheBuilder.newBuilder()
+                // 设置缓存的最大容量
+                .maximumSize(1000)
+                // 最后一次写入后经过固定时间过期
+                .expireAfterWrite(60, TimeUnit.SECONDS)
+                // 最后一次写入或访问后经过固定时间过期
+                .expireAfterAccess(60, TimeUnit.SECONDS)
+                // 设置并发级别，默认为4
+                .concurrencyLevel(10)
+                // 开启统计功能
+                .recordStats()
                 .build();
     }
 
